d528f9bf333042282c12b743b77dc12e51c9d416
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-03-19 20:47:42 +0200

    Add NumbersController

---
+++ Practice/Controllers/NumbersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practice.Controllers
+{
+    public class NumbersController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Tile = "Nums 1 ... 50";
+
+            return View(50);
+        }
+    }
+}
