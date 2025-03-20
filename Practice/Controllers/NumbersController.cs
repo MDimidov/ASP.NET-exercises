663be6694580bc53e6453218ba937c8cc4a5c165
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-03-20 10:30:04 +0200

    Implement NumberToN input field

--- Practice/Controllers/NumbersController.cs
+++ Practice/Controllers/NumbersController.cs
@@ -11,23 +11,28 @@ namespace Practice.Controllers
             return View(50);
         }
 
-        [HttpGet]
-        public IActionResult NumbersToN(int num)
-        {
-            if(num < 1)
-            {
-                return RedirectToAction(nameof(Index), 50);
-            }
+        //[HttpGet]
+        //public IActionResult NumbersToN(int num)
+        //{
+        //    if(num < 1)
+        //    {
+        //        return RedirectToAction(nameof(Index), 50);
+        //    }
 
-            ViewBag.Title = $"Nums 1 ... {num}";
+        //    ViewBag.Title = $"Nums 1 ... {num}";
 
-            return View("Index", num);
-        }
+        //    return View("Index", num);
+        //}
 
         [HttpPost]
         public IActionResult NumbersToN(int num, string test)
         {
-            return View("Index", num);
+            if(num < 1)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(nameof(Index), num);
         }
     }
 }
