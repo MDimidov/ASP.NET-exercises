45ea3b263c9aaaf763a7e0c49a94082fa9e2e4ed
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-03-20 17:14:48 +0200

    Implement Sipmple Chat Messages

---
+++ ChatApp/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,52 @@
+﻿using ChatApp.Models.Chat;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static ICollection<MessageViewModel> messages = new List<MessageViewModel>();
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Show()
+        {
+            if (messages.Count() < 1)
+            {
+                ViewBag.Title = "No Messages yet!";
+            }
+            else
+            {
+                ViewBag.Title = "Messages:";
+            }
+
+                return View(messages);
+        }
+
+        [HttpPost]
+        public IActionResult Send(string author, string message)
+        {
+            int id;
+            if (messages.Count() < 1)
+            {
+                id = 1;
+            } else
+            {
+                id = messages.Max(m => m.Id) + 1;
+            }
+
+            MessageViewModel newChat = new()
+            {
+                Author = author,
+                Message = message,
+                Id = id,
+            };
+
+            messages.Add(newChat);
+            return RedirectToAction(nameof(Show));
+        }
+    }
+}
---
+++ ChatApp/ChatApp/Models/Chat/MessageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApp.Models.Chat
+{
+    public class MessageViewModel
+    {
+        public int Id { get; set; }
+        public required string Message {  get; set; }
+        public required string Author {  get; set; }
+    }
+}
