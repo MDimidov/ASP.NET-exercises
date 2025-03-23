9c0e0cdb5668f4a4d2b0ac0f1a0c0a173c7baa2b
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-03-23 12:47:50 +0200

    Add DbContext & Constants

---
+++ ForumApp/ForumApp.Infrastructure/Data/ForumAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ForumApp.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Infrastructure.Data
+{
+    public class ForumAppDbContext : DbContext
+    {
+        public ForumAppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ ForumApp/ForumApp.Infrastructure/Data/Models/Post.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static ForumApp.Infrastructure.Constants.Constants.PostConstants;
+
+namespace ForumApp.Infrastructure.Data.Models
+{
+    [Comment("Post table")]
+    public class Post
+    {
+        [Key]
+        [Comment("Post identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        [Comment("Post title")]
+        public required string Title { get; set; }
+
+        [Required]
+        [MaxLength(ContentMaxLength)]
+        [Comment("Post content")]
+        public required string Content { get; set; }
+
+    }
+}
+
+
+//•	Id – an unique integer, primary key
+//•	Title – a string with min length 10 and max length 50 (required)
+//•	Content – a string with min length 30 and max length 1500 (required)
