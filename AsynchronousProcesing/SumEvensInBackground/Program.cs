bad91d7070eb2ee67e98497de1cb6e917456dafa
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-03-24 19:27:13 +0200

    Add SumEvensInBackground

---
+++ AsynchronousProcesing/SumEvensInBackground/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+long sum = 0;
+
+Task<long> task = Task.Run(() =>
+{
+    for (long i = 2; i <= 10_000_000_000; i += 2)
+    {
+        sum += i;
+    }
+
+    return sum;
+});
+
+
+string? input;
+Console.Write("Please enter your command: ");
+
+while ((input = Console.ReadLine()) != "show")
+{
+    Console.Write("Please enter your command again: ");
+}
+Console.WriteLine($"Your result is: {task.Result}");
+
