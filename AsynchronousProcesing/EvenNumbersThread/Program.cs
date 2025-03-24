dad382b036e4bab49682d827521df189d574e539
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-03-24 19:02:39 +0200

    Add EvenNumbersThread

---
+++ AsynchronousProcesing/EvenNumbersThread/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int firstNum = int.Parse(Console.ReadLine()!);
+int lastNum = int.Parse(Console.ReadLine()!);
+
+Thread events = new Thread(() => PrintEventNumbers(firstNum, lastNum));
+events.Start();
+events.Join();
+Console.WriteLine("All nums are present!");
+
+void PrintEventNumbers(int firstNum, int lastNum)
+{
+    if (firstNum % 2 != 0)
+    {
+        firstNum++;
+    }
+
+    for (int i = firstNum; i <= lastNum; i += 2)
+    {
+        Console.WriteLine(i);
+    }
+}
