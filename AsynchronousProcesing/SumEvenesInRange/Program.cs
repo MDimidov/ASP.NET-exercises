7be5c7df5b9984269888cc12a1f0295915c05fd6
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-03-24 19:21:41 +0200

    Add SumEvensInRange Async

---
+++ AsynchronousProcesing/SumEvenesInRange/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string? input;
+Console.Write("Please enter your command: ");
+
+while ((input = Console.ReadLine()) != "show")
+{
+    Console.Write("Please enter your command again: ");
+}
+long result = SumAsync();
+Console.WriteLine(result);
+
+
+long SumAsync()
+{
+    return Task.Run(() =>
+    {
+        long sum = 0;
+        for (int i = 2; i <= 1_000_000; i += 2)
+        {
+            sum += i;
+        }
+
+        return sum;
+    }).Result;
+}
