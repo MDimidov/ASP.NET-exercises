9d73ba17407d6f67ddcd06b480ea3fd3b501da93
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-04-06 11:49:55 +0300

    Update Program.cs

--- HouseRentingSystem/HouseRentingSystem/Program.cs
+++ HouseRentingSystem/HouseRentingSystem/Program.cs
@@ -8,7 +8,6 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -16,7 +15,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -27,9 +25,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
 app.MapRazorPages();
 
-app.Run();
+await app.RunAsync();
