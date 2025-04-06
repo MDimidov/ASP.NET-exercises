22771a9f62440414703f468377dc1b00f7a61e92
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-04-06 11:49:05 +0300

    Add ServiceCollectionExtensions

---
+++ HouseRentingSystem/HouseRentingSystem/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using HouseRentingSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDbServiceExtension(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationSerivces(this IServiceCollection services)
+        {
+
+            return services;
+        }
+
+        public static IServiceCollection AddIdentityServiceExtension(this IServiceCollection services)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
