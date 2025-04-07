a0f62222dc2480ba6f6847640f623cadd5cbf57b
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-04-07 13:07:00 +0300

    Implmenet AgentController, Views & Models

---
+++ HouseRentingSystem/HouseRentingSystem/Controllers/AgentController.cs
@@ -0,0 +1,27 @@
+﻿using HouseRentingSystem.Core.Models.Agent;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers
+{
+    [Authorize]
+    public class AgentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Become()
+        {
+            return View(new BecomeAgentFormModel());
+        }
+
+        [HttpPost]
+        public IActionResult Become(BecomeAgentFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction(nameof(HouseController.Index), nameof(HouseController));
+        }
+    }
+}
